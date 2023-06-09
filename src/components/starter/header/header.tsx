f1e9ffd4f73607c7bab7a880ae9e72bc9a1f0a6b
Jeremy Wickersheimer <jwickers@gmail.com>
2023-06-09 15:26:13 +0800

    Reproduction of bug where document events no longer work
    This seems to be caused by the Resource rendering a different DOM element?
    The bug does not occur if the component JSX wraps its output inside a DIV.
    
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,34 +1,175 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikLogo } from '../icons/qwik';
-import styles from './header.module.css';
+import {
+  $,
+  Resource,
+  component$,
+  useOnDocument,
+  useResource$,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+import { QwikLogo } from "../icons/qwik";
+import styles from "./header.module.css";
+
+export const TestComponent = component$(() => {
+  const latestKey = useSignal("");
+
+  useVisibleTask$(({ track }) => {
+    // make sure the suggest dropdown closes when the search is submitted
+    const k = track(() => latestKey.value);
+    console.log("TestComponent::task::latestKey = ", k);
+  });
+  useOnDocument(
+    "click",
+    $((e) => {
+      console.warn("TestComponent::click", e);
+    })
+  );
+  useOnDocument(
+    "keydown",
+    $(async (event) => {
+      const e = event as KeyboardEvent;
+      console.warn("TestComponent::keydown", e);
+      latestKey.value = e.code;
+    })
+  );
+
+  const resource = useResource$<string | null>(async ({ track }) => {
+    const value = track(() => latestKey.value);
+    if (value) {
+      // pause for 1 second
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const res = value.toUpperCase();
+      console.log("TestComponent::useResource::value = ", res);
+      if (res === "ESCAPE") {
+        console.warn(
+          "TestComponent::useResource:: returning an empty string to switch the condition inside the Resource render block, somehow this will cause the events to no longer register."
+        );
+        return "";
+      }
+      return res;
+    }
+    return null;
+  });
+
+  return (
+    <>
+      <Resource
+        value={resource}
+        onPending={() => <>Loading key ...</>}
+        onResolved={(data) => (
+          <>
+            {data ? (
+              <>
+                <div>
+                  <div>
+                    <b>Latest Key: {latestKey.value}</b>
+                  </div>
+                  <div>
+                    <b>Resource data: {data}</b>
+                  </div>
+                </div>
+              </>
+            ) : latestKey.value ? (
+              <>Nothing, you pressed ESCAPE ... and the events no longer work</>
+            ) : (
+              <>Press any key</>
+            )}
+          </>
+        )}
+      />
+    </>
+  );
+});
+
+export const TestComponentWorking = component$(() => {
+  const latestKey = useSignal("");
+
+  useVisibleTask$(({ track }) => {
+    // make sure the suggest dropdown closes when the search is submitted
+    const k = track(() => latestKey.value);
+    console.log("TestComponentWorking::task::latestKey = ", k);
+  });
+  useOnDocument(
+    "click",
+    $((e) => {
+      console.warn("TestComponentWorking::click", e);
+    })
+  );
+  useOnDocument(
+    "keydown",
+    $(async (event) => {
+      const e = event as KeyboardEvent;
+      console.warn("TestComponentWorking::keydown", e);
+      latestKey.value = e.code;
+    })
+  );
+
+  const resource = useResource$<string | null>(async ({ track }) => {
+    const value = track(() => latestKey.value);
+    if (value) {
+      // pause for 1 second
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      const res = value.toUpperCase();
+      console.log("TestComponentWorking::useResource::value = ", res);
+      if (res === "ESCAPE") {
+        console.warn(
+          "TestComponentWorking::useResource:: returning an empty string does not trigger any bug."
+        );
+        return "";
+      }
+      return res;
+    }
+    return null;
+  });
+
+  return (
+    <div>
+      <Resource
+        value={resource}
+        onPending={() => <>Loading key ...</>}
+        onResolved={(data) => (
+          <>
+            {data ? (
+              <>
+                <div>
+                  <div>
+                    <b>Latest Key: {latestKey.value}</b>
+                  </div>
+                  <div>
+                    <b>Resource data: {data}</b>
+                  </div>
+                </div>
+              </>
+            ) : latestKey.value ? (
+              <>Nothing, you pressed ESCAPE ... and the events no longer work</>
+            ) : (
+              <>Press any key</>
+            )}
+          </>
+        )}
+      />
+    </div>
+  );
+});
 
 export default component$(() => {
   return (
     <header class={styles.header}>
-      <div class={['container', styles.wrapper]}>
+      <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
           <a href="/" title="qwik">
             <QwikLogo height={50} width={143} />
           </a>
         </div>
-        <ul>
-          <li>
-            <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-              Docs
-            </a>
-          </li>
-          <li>
-            <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-              Examples
-            </a>
-          </li>
-          <li>
-            <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-              Tutorials
-            </a>
-          </li>
-        </ul>
       </div>
+      <p>
+        Here is a component that listens to Key events and clicks, see the Dev
+        Console. Pressing ESC causes a bug and it no longer reacts to any click
+        or key events.
+      </p>
+      <TestComponent />
+      <p>Here is the same component but its JSX is wrapped in a DIV.</p>
+      <TestComponentWorking />
     </header>
   );
 });
